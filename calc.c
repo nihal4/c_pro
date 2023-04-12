b4e15e04f996d5cee314186843a99d6ad9a76e0e
Nihal <62196755+nihal4@users.noreply.github.com>
2023-04-12 11:25:55 +0600

    ok

--- calc.c
+++ calc.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 
 // void funtion for calculator brain
-void main_Calc(float number_one ,float number_two,char oparations){
-    if (oparations == '+'){
-        printf("\nresult :%f",number_one + number_two);
-    }else if(oparations == '-'){
-        printf("\nresult :%f",number_one - number_two);
-    }else if(oparations == '*'){
-        printf("\nresult :%f",number_one * number_two);
-    }else if(oparations == '/'){
-        printf("\nresult :%f",number_one / number_two);
-    }else{
+void main_Calc(float number_one, float number_two, char oparations) {
+    if (oparations == '+') {
+        printf("\nresult :%f", number_one + number_two);
+    }
+    else if (oparations == '-') {
+        printf("\nresult :%f", number_one - number_two);
+    }
+    else if (oparations == '*') {
+        printf("\nresult :%f", number_one * number_two);
+    }
+    else if (oparations == '/') {
+        printf("\nresult :%f", number_one / number_two);
+    }
+    else {
         printf("syntex erorr");
     }
 }
 
 //main funtion
-int main(){
-    float num1 , num2;
-    char op,dis;
+int main() {
+    float num1, num2;
+    char op, dis;
     int x = 1;
-    while (x){
+    while (x) {
         printf("\ninput number one : ");
-        scanf("%f",&num1);
-        
+        scanf("%f", &num1);
+
         printf("\ninput number two :");
-        scanf("%f",&num2);
+        scanf("%f", &num2);
 
         printf("\ninput oparator (+,-,*,/): ");
-        scanf(" %c",&op);
+        scanf(" %c", &op);
 
-         main_Calc(num1,num2,op);
+        main_Calc(num1, num2, op);
 
         printf("\nfor using calulator again type (y),(e) for exit ");
-        scanf(" %c",&dis);
-        if(dis == 'y'){
+        scanf(" %c", &dis);
+        if (dis == 'y') {
             continue;
-        }else{
+        }
+        else {
             break;
         }
     }
--- gauss_one.c
+++ gauss_one.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
 
 
-void cal(int x1,int y1,int c1,int x2,int y2,int c2){
-    printf("\n%dx+%dy=%d\n",x1,y1,c1);
-    printf("%dx+%dy=%d\n",x2,y2,c2);
-    if (c1 == c2){
+void cal(int x1, int y1, int c1, int x2, int y2, int c2) {
+    printf("\n%dx+%dy=%d\n", x1, y1, c1);
+    printf("%dx+%dy=%d\n", x2, y2, c2);
+    if (c1 == c2) {
         int x = 0;
         int y = 0;
-        printf("x=%d , y=%d",x,y);
+        printf("x=%d , y=%d", x, y);
     }
-    if (y1 == y2){
-        int x = (c1-c2)/(x1-x2);
-        int y = c1-(x*x1);
-        printf("x=%d , y=%d",x,y);
-    }else if(x1==x2){
-        int y = (c1-c2)/(y1-y2);
-        int x = c1-(y*y1);
-        printf("x=%d , y=%d",x,y);
-    }else if (x1!=x2 && y1!=y2){
-        if(x1>x2){
-            x2 = x2*x1; //10
-            y2 = y2*x1; //20
-            c2 = c2*x1; //70
-            if (x1==x2){
-                int y = (c1-c2)/(y1-y2);
-                int x = ((c1-(y*y1))/x1);
-                printf("x=%d , y=%d",x,y);
-            }else if (x1 != x2){
-                x1 = x1*(x2/x1); //5*(10/5) =10
-                y1 = y1*(x2/x1); //3*(10/5) =6
-                c1 = c1*(x2/x1); //21*(10/5) =42
-                int y = (c1-c2)/(y1-y2);
-                int x = ((c1/(x2/x1)) -((y1/(x2/x1))*y))/(x1/(x2/x1));
-                printf("x=%d , y=%d",x,y);
+    if (y1 == y2) {
+        int x = (c1 - c2) / (x1 - x2);
+        int y = c1 - (x * x1);
+        printf("x=%d , y=%d", x, y);
+    }
+    else if (x1 == x2) {
+        int y = (c1 - c2) / (y1 - y2);
+        int x = c1 - (y * y1);
+        printf("x=%d , y=%d", x, y);
+    }
+    else if (x1 != x2) {
+        if (x1 > x2) {
+            x2 = x2 * x1; //10
+            y2 = y2 * x1; //20
+            c2 = c2 * x1; //70
+            if (x1 == x2) {
+                int y = (c1 - c2) / (y1 - y2);
+                int x = ((c1 - (y * y1)) / x1);
+                printf("x=%d , y=%d", x, y);
+            }
+            else if (x1 != x2) {
+                x1 = x1 * (x2 / x1); //5*(10/5) =10
+                y1 = y1 * (x2 / x1); //3*(10/5) =6
+                c1 = c1 * (x2 / x1); //21*(10/5) =42
+                int y = (c1 - c2) / (y1 - y2);
+                int x = (c1 - y * y1) / x1;
+                printf("x=%d , y=%d", x, y);
             }
         }
     }
 }
 
 
-int main(){
-    cal(5,3,21,2,4,14);
+int main() {
+    cal(5, 3, 21, 2, 4, 14);
     return 0;
 }
