b1870cb9d59bdf0efad016420f4110e5805bd75d
Nihal <62196755+nihal4@users.noreply.github.com>
2023-04-12 12:16:57 +0600

    ok

--- gauss_one.c
+++ gauss_one.c
@@ -7,35 +7,35 @@ void cal(int x1, int y1, int c1, int x2, int y2, int c2) {
     if (c1 == c2) {
         int x = 0;
         int y = 0;
-        printf("x=%d , y=%d", x, y);
+        printf("x=%d , y=%d\n", x, y);
     }
     if (y1 == y2) {
         int x = (c1 - c2) / (x1 - x2);
         int y = c1 - (x * x1);
-        printf("x=%d , y=%d", x, y);
+        printf("x=%d , y=%d\n", x, y);
     }
     else if (x1 == x2) {
         int y = (c1 - c2) / (y1 - y2);
         int x = c1 - (y * y1);
-        printf("x=%d , y=%d", x, y);
+        printf("x=%d , y=%d\n", x, y);
     }
     else if (x1 != x2) {
         if (x1 > x2) {
-            x2 = x2 * x1; //10
-            y2 = y2 * x1; //20
-            c2 = c2 * x1; //70
+            x2 = x2 * x1;
+            y2 = y2 * x1;
+            c2 = c2 * x1;
             if (x1 == x2) {
                 int y = (c1 - c2) / (y1 - y2);
                 int x = ((c1 - (y * y1)) / x1);
-                printf("x=%d , y=%d", x, y);
+                printf("x=%d , y=%d\n", x, y);
             }
             else if (x1 != x2) {
-                x1 = x1 * (x2 / x1); //5*(10/5) =10
-                y1 = y1 * (x2 / x1); //3*(10/5) =6
-                c1 = c1 * (x2 / x1); //21*(10/5) =42
-                int y = (c1 - c2) / (y1 - y2);
-                int x = (c1 - y * y1) / x1;
-                printf("x=%d , y=%d", x, y);
+                x1 = x1 * (x2 / x1);
+                y1 = y1 * (x2 / x1);
+                c1 = c1 * (x2 / x1);
+                int y = (c1 - c2) / (y1 - y2);                
+                int x = (c2 - y * y2) / x2;
+                printf("x=%d , y=%d\n", x, y);
             }
         }
     }
@@ -43,6 +43,8 @@ void cal(int x1, int y1, int c1, int x2, int y2, int c2) {
 
 
 int main() {
-    cal(5, 3, 21, 2, 4, 14);
+    cal(1,-1,1, 2, -1, 4);
+    cal(5,3,21, 2, 4, 14);
+    cal(5,4,40,2,2,18);
     return 0;
 }
